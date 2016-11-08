06343f8dbecd207cebac805e028414ab8af3930f
JulianvDoorn <gigajulian@gmail.com>
2016-11-08 15:00:01 +0100

    Ignore .vscode, uploaded __init__

--- main.py
+++ main.py
@@ -1,1 +1,6 @@
-#test
+# dependencies: python-dateutil, httplib2
+
+import nsapi
+
+username = "jerrylooman87@gmail.com"
+password = "ox8ZKmRylP2hf71QCmuq-3_XcKp_iemmoOJFTgyVRaRkDdiZw1d4Fg"
---
+++ nsapi/config.py
@@ -0,0 +1,5 @@
+BaseURL = "webservices.ns.nl"
+
+NSAPIStations = "/ns-api-stations-v2"
+Username = "jerrylooman87@gmail.com"
+Password = "ox8ZKmRylP2hf71QCmuq-3_XcKp_iemmoOJFTgyVRaRkDdiZw1d4Fg"

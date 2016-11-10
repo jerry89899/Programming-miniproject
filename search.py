46ab5c6d9e9b5530086fa2c3e31710fd75dc173b
JulianvDoorn <gigajulian@gmail.com>
2016-11-10 21:37:07 +0100

    "Nieuw station" button added, fixed MultiColumnListBox implementation
    Renamed multiColumnListBox
    Removed commented code
    Added "Nieuw station" button to test stuff
    Fixed MultiColumnListBox implementation
    Removed unnecessary pack_forgets calls

--- search.py
+++ search.py
@@ -1,5 +1,7 @@
 import tkinter as tk
-from multiColumnListBox import MultiColumnListbox
+from multi_column_list_box import MultiColumnListbox
+
+import nsapi.stations as stations
 
 class searchScreen():
 	root = None
@@ -30,13 +32,14 @@ class searchScreen():
 		self.buttonA = tk.Button(self.topFrame, text = "Dit station", height = 4, width = 24, background = "#3F47CC", foreground = "#FFFFFF", font = "bold")
 		self.buttonB = tk.Button(self.topFrame, text = "Ander station", height = 4, width = 24, background = "#3F47CC", foreground = "#FFFFFF", font = "bold")
 
-		# self.entry = AutocompleteEntry([], self.topFrame, width = 1000)
-		# self.entry.pack(side = tk.BOTTOM, pady = (0, 20), padx = (20, 20))
-		
+		self.buttonC = tk.Button(self.topFrame, text = "Nieuw station", height = 4, width = 24, background = "#3F47CC", foreground = "#FFFFFF", font = "bold",
+			command = lambda: self.multiColumnListBox.insert(("Zwoelle", 2,"15:20"))
+		)
+
 		self.entry = tk.Entry(self.topFrame, width=50)
 
-		self.multiColumnListBox = MultiColumnListbox(self.resultsFrame)		
-#		self.entry.pack(side = tk.BOTTOM, pady = (0, 20), padx = (20, 20))
+		self.multiColumnListBox = MultiColumnListbox(self.resultsFrame, ["Bestemming", "Spoor","Tijd"])	
+
 	def show(self, returnScreen = None):
 		self.returnScreen = returnScreen
 
@@ -47,17 +50,17 @@ class searchScreen():
 		self.annuleerButton.pack(pady = 20, padx = (20, 0), side = tk.LEFT)
 		self.buttonA.pack(pady = 20, padx = (20, 0), side = tk.LEFT)
 		self.buttonB.pack(pady = 20, padx = (20, 0), side = tk.LEFT)
+		self.buttonC.pack(pady = 20, padx = (20, 0), side = tk.LEFT)
+
+		self.multiColumnListBox.clear()
+
+		for i in stations.getDepartures("Utrecht Centraal"):
+			self.multiColumnListBox.insert((i["Destination"], i["DeparturePlatform"], i["DepartureTime"]))
+
 		self.entry.pack(side = tk.BOTTOM, pady = (0, 20), padx = (20, 20))
 
 	def hide(self):
 		self.searchFrame.pack_forget()
-		self.topFrame.pack_forget()
-		self.resultsFrame.pack_forget()
-
-		self.annuleerButton.pack_forget()
-		self.buttonA.pack_forget()
-		self.buttonB.pack_forget()
-		self.entry.pack_forget()
 
 		if self.returnScreen != None:
 			self.returnScreen.show()

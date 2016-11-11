1e10190de097da20ad3f91837046206588083705
JulianvDoorn <gigajulian@gmail.com>
2016-11-11 17:27:01 +0100

    Fixed Conflicts

--- station_selector.py
+++ station_selector.py
@@ -1,32 +1,15 @@
 import tkinter as tk
-<<<<<<< HEAD
-from all_stations import all_stations as stations
 
-class SelectorScreen():
-	root = None
-	returnScreen = None
-	
-	# Frames
-	primaryFrame = None
-	controlFrame = None
-	bottomFrame = None
-	
-	# Buttons
-	buttonA = None
-=======
-# from search import SearchScreen
-from page_stack import stack as page_stack
 from tkinter import *
 import re
 from allstations import all_stations as stations
-from autofillgui import AutocompleteEntry as autocomplete
+from autofillgui import AutocompleteEntry
 
 extracted_stations = []
 for v in stations:
     extracted_stations.append(v["Names"]["Long"])
     if "Synonyms" in v:
         extracted_stations.extend(v["Synonyms"])
-
 
 class SelectorScreen():
     root = None
@@ -39,46 +22,38 @@ class SelectorScreen():
 
     # Buttons
     buttonA = None
->>>>>>> refs/remotes/origin/master
 
     def __init__(self, root):
         self.root = root
 
-<<<<<<< HEAD
-		self.primaryFrame = tk.Frame(self.root)
-
-		self.controlFrame = tk.Frame(self.primaryFrame, background = "#FFD61E")
-		self.stationsFrame = tk.Frame(self.primaryFrame, background = "#FFD61E")
-
-		self.buttonA = tk.Button(self.controlFrame, text = "Annuleer", height = 4, width = 24, background = "#3F47CC", foreground = "#FFFFFF", font = "bold", command = self.hide)
-		self.buttonB = tk.Button(self.controlFrame, text = "Huidig station", height = 4, width = 24, background = "#3F47CC", foreground = "#FFFFFF", font = "bold")
-=======
         self.primaryFrame = tk.Frame(self.root)
         self.filterFrame = tk.Frame(self.primaryFrame, background="#FFD61E")
         self.stationsFrame = tk.Frame(self.primaryFrame, background="#FFD61E")
         self.bottomFrame = tk.Frame(self.primaryFrame, background="#FFD61E")
-        self.entry = tk.Entry(self.filterFrame, width=50, background="#3F47CC")
-        self.buttonA = tk.Button(self.filterFrame, text="Annuleer", height=4, width=24, background="#3F47CC",
-                                 foreground="#FFFFFF", font="bold", command=self.hide)
+        self.buttonA = tk.Button(self.filterFrame, text="Annuleer", height=4, width=24, background="#3F47CC", foreground="#FFFFFF", font="bold", command=self.hide)
+        self.zoek = tk.Button(self.filterFrame, text="Zoek", height=4, width=24, background="#3F47CC", foreground="#FFFFFF", font="bold", command=self.doSearch)
+
+        self.entry = AutocompleteEntry(extracted_stations, self.filterFrame, width = 100)
+
+    def doSearch(self):
+        self._hide()
+        station = self.entry.var.get()
+        self.returnScreen.show(None, station)
 
     def show(self, returnScreen=None):
         if returnScreen != None:
             self.returnScreen = returnScreen
->>>>>>> refs/remotes/origin/master
 
         self.primaryFrame.pack(fill=tk.BOTH, expand=True)
 
         self.filterFrame.pack(side=tk.TOP, fill=tk.BOTH, expand=True)
 
-<<<<<<< HEAD
-		self.controlFrame.pack(side = tk.TOP, fill = tk.BOTH)
-		self.stationsFrame.pack(side = tk.TOP, fill = tk.BOTH, expand = True)
-=======
-        self.entry.pack(side=tk.TOP, expand=True)
+        self.entry.pack(side = tk.TOP, expand = True)
+
         self.stationsFrame.pack(side=tk.TOP, fill=tk.BOTH, expand=True)
         self.bottomFrame.pack(side=tk.TOP, fill=tk.BOTH, expand=True)
         self.buttonA.pack(pady=20, padx=(20, 0), side=tk.LEFT)
->>>>>>> refs/remotes/origin/master
+        self.zoek.pack(pady=20, padx=(20, 0), side=tk.LEFT)
 
     def hide(self):
         self._hide()
@@ -88,5 +63,3 @@ class SelectorScreen():
 
     def _hide(self):
         self.primaryFrame.pack_forget()
-
-autocomplete(extracted_stations)

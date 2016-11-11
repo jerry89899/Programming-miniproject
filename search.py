4c6fd4010ac224ff10dca32de01db37c7ef40655
JulianvDoorn <gigajulian@gmail.com>
2016-11-11 14:00:10 +0100

    Changed Variable Names and Go Back

--- search.py
+++ search.py
@@ -1,14 +1,16 @@
 import tkinter as tk
-from multi_column_list_box import MultiColumnListbox
+from multi_column_list_box import MultiColumnListBox
+from station_selector import SelectorScreen
+from page_stack import stack as page_stack
 
 import nsapi.stations as stations
 
-class searchScreen():
+class SearchScreen():
 	root = None
 	returnScreen = None
 	
 	# Frames
-	searchFrame = None
+	primaryFrame = None
 	topFrame = None
 	resultsFrame = None
 	entry = None
@@ -24,43 +26,45 @@ class searchScreen():
 	def __init__(self, root):
 		self.root = root
 
-		self.searchFrame = tk.Frame(self.root)
-		self.topFrame = tk.Frame(self.searchFrame, background = "#FFD61E")
-		self.resultsFrame = tk.Frame(self.searchFrame, background = "#FFD61E")
+		self.primaryFrame = tk.Frame(self.root)
+		self.topFrame = tk.Frame(self.primaryFrame, background = "#FFD61E")
+		self.resultsFrame = tk.Frame(self.primaryFrame, background = "#FFD61E")
 
 		self.annuleerButton = tk.Button(self.topFrame, text = "Annuleer", height = 4, width = 24, background = "#3F47CC", foreground = "#FFFFFF", font = "bold", command = self.hide)
 		self.buttonA = tk.Button(self.topFrame, text = "Dit station", height = 4, width = 24, background = "#3F47CC", foreground = "#FFFFFF", font = "bold")
-		self.buttonB = tk.Button(self.topFrame, text = "Ander station", height = 4, width = 24, background = "#3F47CC", foreground = "#FFFFFF", font = "bold")
-
-		self.buttonC = tk.Button(self.topFrame, text = "Nieuw station", height = 4, width = 24, background = "#3F47CC", foreground = "#FFFFFF", font = "bold",
-			command = lambda: self.multiColumnListBox.insert(("Zwoelle", 2,"15:20"))
-		)
+		self.buttonB = tk.Button(self.topFrame, text = "Ander station", height = 4, width = 24, background = "#3F47CC", foreground = "#FFFFFF", font = "bold", command = self.open_station_selector)
 
 		self.entry = tk.Entry(self.topFrame, width=50)
 
-		self.multiColumnListBox = MultiColumnListbox(self.resultsFrame, ["Bestemming", "Spoor","Tijd"])	
+		self.multiColumnListBox = MultiColumnListBox(self.resultsFrame, ["Bestemming", "Spoor","Tijd"])	
+		self.multiColumnListBox.clear()
+
+		for i in stations.getDepartures("Utrecht Centraal"):
+			self.multiColumnListBox.insert((i["Destination"], i["DeparturePlatform"], i["DepartureTime"]))
+
+	def open_station_selector(self):
+		self._hide()
+		SelectorScreen(self.root).show(self)
 
 	def show(self, returnScreen = None):
-		self.returnScreen = returnScreen
+		if returnScreen != None:
+			self.returnScreen = returnScreen
 
-		self.searchFrame.pack(fill = tk.BOTH, expand = True)
+		self.primaryFrame.pack(fill = tk.BOTH, expand = True)
 		self.topFrame.pack(side = tk.TOP, fill = tk.BOTH)
 		self.resultsFrame.pack(side = tk.TOP, fill = tk.BOTH, expand = True)
 		
 		self.annuleerButton.pack(pady = 20, padx = (20, 0), side = tk.LEFT)
 		self.buttonA.pack(pady = 20, padx = (20, 0), side = tk.LEFT)
 		self.buttonB.pack(pady = 20, padx = (20, 0), side = tk.LEFT)
-		self.buttonC.pack(pady = 20, padx = (20, 0), side = tk.LEFT)
-
-		self.multiColumnListBox.clear()
-
-		for i in stations.getDepartures("Utrecht Centraal"):
-			self.multiColumnListBox.insert((i["Destination"], i["DeparturePlatform"], i["DepartureTime"]))
-
+	
 		self.entry.pack(side = tk.BOTTOM, pady = (0, 20), padx = (20, 20))
 
 	def hide(self):
-		self.searchFrame.pack_forget()
+		self._hide()
 
 		if self.returnScreen != None:
 			self.returnScreen.show()
+
+	def _hide(self):
+		self.primaryFrame.pack_forget()
